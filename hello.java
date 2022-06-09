1d9c77730787d46ac7e74d4e0b73244803848d3f
Prasad <prasadj007official@gmail.com>
2022-06-09 12:57:19 +0530

    Print Program with Hello World

--- hello.java
+++ hello.java
@@ -1,5 +1,5 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("This will be printed Hello");
+        System.out.println("This will be printed Hello World");
     }
 }
